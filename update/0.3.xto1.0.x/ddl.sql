439b8b95b193b7e9526b02d21e17444ede94425c
blackholll <blackholll@163.com>
2020-03-24 08:04:23 +0800

    version update script

---
+++ update/0.3.xto1.0.x/ddl.sql
@@ -0,0 +1,54 @@
+-- LoonUser 新增字段is_workflow_admin
+alter table account_loonuser add is_workflow_admin TINYINT(1) UNSIGNED NOT NULL default 0 comment '工作流管理员'
+
+-- CustomNotice 字段修改， 去掉script，title_template， content_template  新增hook_url， hook_token 执行sql:
+alter table workflow_customnotice add hook_url varchar(100) not null default '' comment 'hook_url';
+alter table workflow_customnotice add hook_token varchar(100) not null default '' comment 'hook_token';
+alter table workflow_customnotice drop column script;
+alter table workflow_customnotice drop column hook_token;
+
+-- Workflow表新增 title_template， content_template
+alter table workflow_workflow add title_template varchar(100) not null default '' comment '标题模板';
+alter table workflow_workflow add content_template varchar(1000) not null default '' comment '内容模板';
+
+--新增WorkflowAdmin表，用于保存工作流管理员
+CREATE TABLE `workflow_workflowadmin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creator` varchar(50) NOT NULL,
+  `gmt_created` datetime(6) NOT NULL,
+  `gmt_modified` datetime(6) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `workflow_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_workflow_id` (`workflow_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 删除workflow表中的flowchart字段
+alter table workflow_workflow drop column flowchart;
+
+--state表中删除sub_workflow_id字段
+alter table workflow_state drop column sub_workflow_id;
+
+--工单基础表中 删除is_end、 is_rejected字段
+alter table ticket_ticketrecord drop column is_end;
+alter table ticket_ticketrecord drop column is_rejected;
+
+-- 工单基础表中新增act_state_id int类型， 分别为草稿中0 进行中1 被拒绝2  被撤回3 已完成4
+ALTER TABLE `ticket_ticketrecord`
+	ADD COLUMN `act_state_id` INT NOT NULL DEFAULT '0' COMMENT '进行状态' AFTER `script_run_last_result`
+
+-- 工单关系人表
+CREATE TABLE `ticket_ticketuser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creator` varchar(50) NOT NULL,
+  `gmt_created` datetime(6) NOT NULL,
+  `gmt_modified` datetime(6) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `in_process` tinyint(1) NOT NULL,
+  `ticket_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_ticket_id` (`ticket_id`),
+  KEY `idx_username_in_process` (`username`,`in_process`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
---
+++ update/0.3.xto1.0.x/dml.py
@@ -0,0 +1,89 @@
+import MySQLdb
+
+# 数据修改
+DB_HOST = '127.0.0.1'
+DB_NAME = 'loonflownew1'
+DB_PORT = 3306
+DB_USER = 'loonflownew1'
+DB_PASSWORD = '123456'
+
+COUNT_PER_TIME = 1000  # 每次处理1000条数据
+
+# 当前处理人、历史处理人迁移
+# 查询总行数
+
+db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD, db=DB_NAME, port=DB_PORT, charset='utf8' )
+cursor = db.cursor()
+
+cursor.execute("select count(*) from ticket_ticketrecord")
+
+count_query = cursor.fetchone()
+count_result = count_query[0]
+print('共{}条记录'.format(count_result))
+# 获取relation记录插入， 然后获取当前处理人(个人或者多人的情况)来更新relation记录
+
+if count_result % COUNT_PER_TIME:
+    total_time = int((count_result/COUNT_PER_TIME)) + 1
+else:
+    total_time = int(count_result/COUNT_PER_TIME)
+
+for i in range(total_time):
+    limit_start = i
+    limit_end = i + COUNT_PER_TIME
+    raw_sql0 = "select id, relation,participant_type_id, participant, gmt_created, state_id from ticket_ticketrecord " \
+               "limit {},{}".format(limit_start, limit_end)
+    cursor.execute(raw_sql0)
+    result = cursor.fetchall()
+
+    # 插入relation记录
+    insert_relation_sql_list = []
+    update_relation_sql_list = []
+    update_ticket_sql_list = []
+    for result0 in result:
+        ticket_id, relation, participant_type_id, participant, gmt_created, state_id = result0
+        if relation:
+            for relation0 in relation.split(','):
+                insert_relation_sql_list.append(
+                    "insert into ticket_ticketuser (ticket_id, gmt_created, gmt_modified,creator, username, in_process,"
+                    "is_deleted) value ({},'{}','{}','{}','{}', 0, 0)".format(ticket_id, gmt_created, gmt_created,
+                                                                              'admin', relation0))
+        if participant_type_id == 1:
+            update_relation_sql_list.append(
+                "update ticket_ticketuser set in_process=1 where ticket_id={} and username='{}' and is_deleted=0"
+                    .format(ticket_id, participant))
+        if participant_type_id == 2:
+            for participant0 in participant.split(','):
+                if participant0:
+                    update_relation_sql_list.append(
+                        "update ticket_ticketuser set in_process=1 where ticket_id={} and username='{}' and "
+                        "is_deleted=0".format(ticket_id, participant0))
+
+        # ticket record中attr_state_id变更， 需要查询flow_log以及当前state_id来做处理 草稿中0 进行中1 被拒绝2  被撤回3 已完成4
+        # 0.3版本不支持保存到初始状态，所以不会有草稿中， 如果最后一次操作是拒绝类型的，那么attr_state_id为被拒绝， 如果最后一次操作非拒绝，且状态非结束状态，那么attr_state_id为进行中，
+        # 如果当前状态为已经结束，那么attr_state_id为已完成
+        last_transition_query_sql = "select transition_type_id from workflow_transition where id =(select transition_id from ticket_ticketflowlog where id =(select max(id) from ticket_ticketflowlog where ticket_id={}))".format(ticket_id)
+        cursor.execute(last_transition_query_sql)
+        last_transition_type_id = cursor.fetchone()[0]
+        if last_transition_type_id == 2:  # 被拒绝
+            attr_state_id = 2  # 工单的进行状态为:被退回
+        else:
+            ticket_state_query_sql = "select type_id from workflow_state where id={}".format(state_id)
+            cursor.execute(ticket_state_query_sql)
+            ticket_state_type_id = cursor.fetchone()[0]
+            if ticket_state_type_id == 0:  # 非结束状态
+                attr_state_id = 1  # 工单的进行状态为:进行中
+            else:
+                attr_state_id = 4  # 工单的进行状态为已完成
+        update_ticket_sql_list.append("update ticket_ticketrecord set attr_state_id={} where id={}".format(attr_state_id, ticket_id))
+
+    insert_relation_sql = ';'.join(insert_relation_sql_list)
+    update_relation_sql = ';'.join(update_relation_sql_list)
+    update_ticket_sql = ';'.join(update_ticket_sql_list)
+    cursor.execute(insert_relation_sql)
+    cursor.execute(update_relation_sql)
+    cursor.execute(update_ticket_sql)
+
+db.close()
+
+
+
